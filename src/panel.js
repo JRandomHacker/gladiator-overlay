b69ec48d05a626cd1191eaa2bcf393fa1597ac72
Justin Koehler <jrandomhacker172342@gmail.com>
2022-02-01 23:06:04 -0600

    Populate dropdown with match names

--- src/panel.js
+++ src/panel.js
@@ -1,8 +1,15 @@
 var streamTitle = nodecg.Replicant('streamTitle', {defaultValue: "GLADIATOR LEAGUE TOURNAMENTS"});
+var matches = nodecg.Replicant('matches', {defaultValue: []});
+
 var streamTitleInput = document.getElementById('stream-title-input');
 streamTitleInput.onchange = function() {
     streamTitle.value = streamTitleInput.value;
 }
+
+matches.on('change', function(newVal, oldVal) {
+    var matchesList = document.getElementById('polymer-head');
+    matchesList.matchNames = newVal.map(match => match.Player1Username + " - " + match.Player2Username);
+});
 
 document.getElementById('update-pairings').onclick = updatePairings;
 
@@ -11,4 +18,5 @@ function updatePairings() {
     var eventId = document.getElementById('event-id').value;
 
     nodecg.sendMessage('test', eventId);
+
 }
