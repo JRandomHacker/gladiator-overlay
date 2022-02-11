aa12010ef37da21fa0caee3a36c93dfff6caae0b
Justin Koehler <jrandomhacker172342@gmail.com>
2022-02-10 21:30:50 -0600

    Finish CSV-parsed matches

--- src/graphic.js
+++ src/graphic.js
@@ -1,7 +1,15 @@
 onload = function() {
-    var streamTitle = nodecg.Replicant('streamTitle', {defaultValue: "GLADIATOR LEAGUE TOURNAMENTS"});
+    var streamTitle = nodecg.Replicant('streamTitle');
+    var currentMatch = nodecg.Replicant('currentMatch');
 
-    streamTitle.on('change', function(newVal, oldVal) {
+    streamTitle.on('change', function(newVal) {
         document.getElementById('maintitle').firstChild.textContent = newVal;
     });
+
+	currentMatch.on('change', function(newVal) {
+        document.getElementById('pov-name').firstChild.textContent = newVal.povName;
+        document.getElementById('pov-decklist').firstChild.textContent = newVal.povDecklist;
+        document.getElementById('opponent-name').firstChild.textContent = newVal.opponentName;
+        document.getElementById('opponent-decklist').firstChild.textContent = newVal.opponentDecklist;
+	});
 }
--- src/panel.js
+++ src/panel.js
@@ -1,4 +1,11 @@
 var streamTitle = nodecg.Replicant('streamTitle', {defaultValue: "GLADIATOR LEAGUE TOURNAMENTS"});
+var currentMatch = nodecg.Replicant('currentMatch', {defaultValue: 
+	{
+		povName: "POV Name",
+		povDecklist: "POV Decklist",
+		opponentName: "Opponent Name",
+		opponentDecklist: "Opponent Decklist"
+	}});
 var matches = nodecg.Replicant('matches', {defaultValue: []});
 
 var streamTitleInput = document.getElementById('stream-title-input');
@@ -12,6 +19,7 @@ matches.on('change', function(newVal, oldVal) {
 });
 
 document.getElementById('update-pairings').onclick = updatePairings;
+document.getElementById('update-overlay').onclick = updateOverlay;
 
 
 function updatePairings() {
@@ -19,4 +27,29 @@ function updatePairings() {
 
     nodecg.sendMessage('test', eventId);
 
+}
+
+function updateOverlay() {
+	var matchesDropdown = document.getElementById('feature-matches');
+	var flipToggle = document.getElementById('flip-toggle');
+	var selectedMatch = matches.value[matchesDropdown.selected];
+
+	if (flipToggle.checked)
+	{
+		currentMatch.value = {
+			povName: selectedMatch.Player2Username.toUpperCase(),
+			povDecklist: selectedMatch.Player2Decklist.toUpperCase(),
+			opponentName: selectedMatch.Player1Username.toUpperCase(),
+			opponentDecklist: selectedMatch.Player1Decklist.toUpperCase(),
+		};
+	}
+	else
+	{
+		currentMatch.value = {
+			povName: selectedMatch.Player1Username.toUpperCase(),
+			povDecklist: selectedMatch.Player1Decklist.toUpperCase(),
+			opponentName: selectedMatch.Player2Username.toUpperCase(),
+			opponentDecklist: selectedMatch.Player2Decklist.toUpperCase(),
+		};
+	}
 }
